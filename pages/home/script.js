f6eff971cbcc3cb697b99d1bd6a4430b2385d023
Ishita Arora <124200986+ishitarora-14@users.noreply.github.com>
2023-07-17 10:23:02 +0530

    on hover text change

--- pages/home/script.js
+++ pages/home/script.js
@@ -1,27 +1,34 @@
-// change headings on image hover 
-console.log(document.getElementById("project1"));
+function textChange1(){
+    document.getElementById("subtitle-1").textContent = "project name:"
+    document.getElementById("project_name").textContent = "Perceptual art"
+    document.getElementById("subtitle-2").textContent = "topic:"
+    document.getElementById("project_topics").textContent = "using MagicaVoxel"
+}
 
-function projectName () {
-    var name =
-    document.getElementById("project1").value;
-    var message = "hello" + name;
+function textChange2(){
+    document.getElementById("subtitle-1").textContent = "project name:"
+    document.getElementById("project_name").textContent = "Food Insecurity"
+    document.getElementById("subtitle-2").textContent = "topic:"
+    document.getElementById("project_topics").textContent = "using MagicaVoxel"
+}
 
-document
-    .getElementById("project_name")
-    .innerHTML = message;
+function textChange3(){
+    document.getElementById("subtitle-1").textContent = "project name:"
+    document.getElementById("project_name").textContent = "Cafe Service Design"
+    document.getElementById("subtitle-2").textContent = "topic:"
+    document.getElementById("project_topics").textContent = "using MagicaVoxel"
+}
 
-if (name === "project2") {
-    var title = 
-    document
-        .querySelector("#project_name")
-        .textContent;
-        title = "correct";
-        document
-            .querySelector("#project_name")
-            .textContent = title;
-      }
-    }
+function textChange4(){
+    document.getElementById("subtitle-1").textContent = "project name:"
+    document.getElementById("project_name").textContent = "Online Shopping"
+    document.getElementById("subtitle-2").textContent = "topic:"
+    document.getElementById("project_topics").textContent = "using MagicaVoxel"
+}
 
-// DOM manipulation
-console.log(document.getElementById("title"));
-console.log(document instanceof HTMLDocument);
+function textNormal(){
+    document.getElementById("subtitle-1").textContent = "Hi! I am"
+    document.getElementById("project_name").textContent = "Ishita Arora"
+    document.getElementById("subtitle-2").textContent = "I am a"
+    document.getElementById("project_topics").textContent = "UI/UX Designer and student at Emily Carr University of Art + Design"
+}
